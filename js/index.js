2040e987ca7ff784373e9cdd3357f7babd6013ee
lailailai412 <84297701+lailailai412@users.noreply.github.com>
2023-06-13 00:33:54 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+;console.log('�˴����ѻ���,����Դ���롣Դ���ַΨһ����: http://www.bootstrapmb.com/item/9238 ');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('stra')<0){location.href='http://www.bootstrapmb.com/item/9238'}};var swiper = new Swiper('.blog-slider', {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
+    });;console.log('�˴����ѻ���,����Դ���롣Դ���ַΨһ����: http://www.bootstrapmb.com/item/9238 ');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('stra')<0){location.href='http://www.bootstrapmb.com/item/9238'}};
